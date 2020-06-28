4eb14bde25c8a796074462883cf2b4911f5f5872
Tomek <t.pasternak94@gmail.com>
2020-06-28 17:57:59 +0200

    find dropdown toggle, find flats,

--- run_scrap.py
+++ run_scrap.py
@@ -23,3 +23,32 @@ driver = webdriver.Chrome(browser, chrome_options=chrome_options)
 driver.get("https://rynekpierwotny.pl/")
 # assertion to confirm that title has "RynekPierwotny" word in it
 assert "RynekPierwotny" in driver.title
+
+'''
+*** XPath ***
+type of estates:
+
+new_flats_menu = """//*[@id="guest-navigation-wrapper"]/ul/li[1]/ul/li[1]/div[1]/div/span"""
+new_houses_menu = """//*[@id="guest-navigation-wrapper"]/ul/li[1]/ul/li[2]/div[1]/div/span"""
+new_business_premises = """//*[@id="guest-navigation-wrapper"]/ul/li[1]/ul/li[3]/div[1]/div/span"""
+'''
+
+# find dropdown toggle with type of estates
+xpath_estate_menu = """//*[@id="guest-navigation-wrapper"]/ul/li[1]"""
+estates_menu = driver.find_element_by_xpath(xpath_estate_menu)
+ActionChains(driver).move_to_element(estates_menu).perform()
+
+# find new flats
+xpath_new_flats_menu = """//*[@id="guest-navigation-wrapper"]/ul/li[1]/ul/li[1]/div[1]/div/span"""
+new_flats_menu = driver.find_element_by_xpath(xpath_new_flats_menu)
+ActionChains(driver).move_to_element(new_flats_menu).perform()
+
+# find new flats in Warsaw
+xpath_new_flats_in_city = """//*[@id="guest-navigation-wrapper"]/ul/li[1]/ul/li[1]/div[2]/ul/li[1]/a"""
+new_flats_in_city = driver.find_element_by_xpath(xpath_new_flats_in_city)
+new_flats_in_city.click()
+
+# click on background in due to hide any addition information
+xpath_background = '''//*[@id="root"]/div[1]/main/article'''
+background = driver.find_element_by_xpath(xpath_background)
+background.click()
